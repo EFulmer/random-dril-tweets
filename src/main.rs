c62fb0e8df0808c30b98264e284cd58da1c3ae4a
Eric Fulmer <eric.r.fulmer@gmail.com>
2016-07-19 21:45:51 -0400

    added in mongo fns

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
+#[macro_use(bson, doc)]
+extern crate bson;
+extern crate mongodb;
+
 extern crate ansi_term;
 extern crate rand;
 
 use std::env;
+use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::{Read, Write};
 use std::path::Path;
 use std::process::Command;
+use std::time::SystemTime;
+use std::time::Duration;
 
 use ansi_term::Colour::Blue;
+use mongodb::{Client, ThreadedClient};
+use mongodb::db::ThreadedDatabase;
 
 const DEFAULT_GREPTWEETS_URL: &'static str = "http://greptweet.com/u/dril/dril.txt";
 const DEFAULT_TWEET_FILE: &'static str = "dril.txt";
 
-fn get_dril_tweets(url: &str, out_file_name: &str) -> io::Result<()> {
+fn get_tweets(url: &str, out_file_name: &str) -> io::Result<()> {
     let output = try!(Command::new("curl")
         .arg("--compressed")
         .arg("-s")
@@ -23,6 +32,28 @@ fn get_dril_tweets(url: &str, out_file_name: &str) -> io::Result<()> {
     let mut f = File::create(out_file_name).unwrap();
 
     try!(f.write(output.stdout.as_slice()));
+    Ok(())
+}
+
+fn add_tweets_mongo(file_name: &str) -> io::Result<()> {
+    let mut buf = String::new();
+    let mut f = File::open(file_name).unwrap();
+    try!(f.read_to_string(&mut buf));
+
+    let client = Client::connect("localhost", 27017).ok().expect("Failed to initialize client.");
+    let coll = client.db("dril").collection("tweets");
+    for line in buf.lines() {
+        let tweet = line.split('|').last().unwrap();
+        let doc = doc!{"tweet" => tweet};
+        let mut cursor = coll.find(Some(doc), None).unwrap();
+        if cursor.count() == 0 {
+            // TODO errhand
+            coll.insert_one(doc!{"tweet" => tweet}, None).unwrap();
+        } else { 
+            break;
+        }
+    }
+
     Ok(())
 }
 
@@ -41,6 +72,19 @@ fn random_tweet(tweet_file: &str) -> io::Result<String> {
     Ok(tweets[which_tweet].to_owned())
 }
 
+fn random_tweet_mongo() -> io::Result<String> {
+    let client = Client::connect("localhost", 27017).ok().expect("Failed to initialize client.");
+    let coll = client.db("dril").collection("tweets");
+
+    let mut cursor = coll.find(None, None).unwrap();
+    let num_tweets = coll.find(None, None).unwrap().count();
+
+    let which_tweet: usize = rand::random::<usize>() % num_tweets;
+
+    let tweet = cursor.skip(which_tweet).next().unwrap().unwrap();
+    Ok(tweet.get_str("tweet").unwrap().to_owned())
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     let tweets_url: &str;
@@ -54,15 +98,24 @@ fn main() {
         tweets_txt = DEFAULT_TWEET_FILE;
     }
 
-    // would use fs::Metadata::modified, but that's currently flagged as unstable.
-    if !(Path::new(tweets_txt).exists()) {
-        if let Err(e) = get_dril_tweets(tweets_url, tweets_txt) {
-            println!("Error encountered retrieving tweets: {:?}", e);
+    if let Ok(metadata) = fs::metadata(tweets_txt) {
+        if let Ok(last_modified) = metadata.modified() {
+            if let Ok(diff) = SystemTime::now().duration_since(last_modified) {
+                if diff >= Duration::from_secs(172800) {
+                    if let Err(e) = get_tweets(tweets_url, tweets_txt) {
+                        println!("Error encountered retrieving tweets: {:?}", e);
+                    }
+                }
+            }
         }
+    } else if let Err(e) = get_tweets(tweets_url, tweets_txt) {
+        println!("Error encountered retrieving tweets: {:?}", e);
     }
 
     match random_tweet(tweets_txt) {
         Err(e) => println!("Error encountered reading tweet file: {:?}", e),
         Ok(tweet) => println!("{}", Blue.paint(tweet)),
     };
+
+    add_tweets_mongo(tweets_txt);
 }
